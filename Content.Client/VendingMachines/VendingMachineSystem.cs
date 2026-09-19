94d2c195b292d90d75e31cea8fea0fc8dca3849d
Lytheriia <289052288+Lytheriia@users.noreply.github.com>
2026-09-19 12:21:12 +0200

    fix

--- Content.Client/VendingMachines/VendingMachineSystem.cs
+++ Content.Client/VendingMachines/VendingMachineSystem.cs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using System.Linq;
-using Content.Client._Reserve.VendingMachines
+using Content.Client._Reserve.VendingMachines;
 using Content.Shared.VendingMachines;
 using Robust.Client.Animations;
 using Robust.Client.GameObjects;
