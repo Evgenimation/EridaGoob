a56f2e49b39d0a9939177e65ad8c217140e935cb
Lytheriia <289052288+Lytheriia@users.noreply.github.com>
2026-09-20 04:27:02 +0200

    i hate that

--- Content.Shared/_Erida/CCVar/CCVars.Api.cs
+++ Content.Shared/_Erida/CCVar/CCVars.Api.cs
@@ -9,8 +9,8 @@ namespace Content.Shared._Erida.CCVar;
 public sealed partial class ECCVars
 {
     public static readonly CVarDef<string> ApiToken =
-        CVarDef.Create("eapi.api_token", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
+        CVarDef.Create("eapi.key", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
 
     public static readonly CVarDef<string> ApiUrl =
-        CVarDef.Create("eapi.api_url", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
+        CVarDef.Create("eapi.url", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
 }
